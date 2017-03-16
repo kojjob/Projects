13cbeec773f24dd888d9c5bf597f6463812dc141
Kojjob <kodeon6@gmail.com>
2017-03-16 19:57:03 +0000

    Route modified

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-
-  get 'pages/about'
-
-  get 'pages/contact'
 
   resources :portfolios
+
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+  
   resources :skills
   resources :blogs
-  root 'blogs#index'
+  
+  root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

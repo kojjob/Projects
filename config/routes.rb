9b33f45f2286da1c9eaa20f7bf96f4fbe4820741
Kojjob <kodeon6@gmail.com>
2017-03-17 00:15:19 +0000

    Add Draft and Published functionality

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,11 @@ Rails.application.routes.draw do
 
   resources :portfolios
   resources :skills
-  resources :blogs
+  resources :blogs do
+  	member do
+  		get :toggle_status
+  	end
+  end
   root 'blogs#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
